4dfcb3a58a122228468f4b712348aba5b1493110
Prashant Singh <prashantsingh9452180588@gmaul.com>
2025-08-28 03:57:11 +0530

    trying css changes1

--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -184,7 +184,7 @@ Rules:
     <div className='sm:px-10 md:px-32 lg:px-56 xl:px-72 px-5 mt-10'>
       
         <h2 className='font-bold text-3xl'>
-          Tell us your travel preferences🌍🌴
+          Tell us your travel preferences🌴
         </h2>
 
         <p className='mt-3 text-gray-500 text-xl'>Just provide some basic information, and our trip planner will generate a customized itinerary based on your preferences.
