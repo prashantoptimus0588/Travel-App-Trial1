995d667c35be3854f17bf3ba5c9b7ad200155039
Prashant Singh <prashantsingh9452180588@gmaul.com>
2025-08-30 11:34:07 +0530

    Hero Page Image Change

--- src/components/ui/custom/Hero.jsx
+++ src/components/ui/custom/Hero.jsx
@@ -17,7 +17,7 @@ const Hero = () => {
         </Button>
       </Link>
       <img 
-        src="/beach.jpg" 
+        src="/person-traveling-enjoying-their-vacation.jpg" 
         alt="Time to Travel" 
         className='mt-12 w-full max-w-4xl rounded-3xl shadow-2xl transition-transform duration-500 hover:scale-[1.02] object-cover h-auto'
       />
